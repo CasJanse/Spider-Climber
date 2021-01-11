b1328b2009791957930fb23553f0d1478e81ccf5
CasJanse <cas.janse97@gmail.com>
2021-01-11 09:49:16 +0100

    Add working Inverse Kinematic. Rotation of bones is broken

--- Assets/Scripts/FastIKFabric.cs
+++ Assets/Scripts/FastIKFabric.cs
@@ -19,7 +19,10 @@ public class FastIKFabric : MonoBehaviour
     protected float completeLength;
     protected Transform[] bones;
     protected Vector3[] positions;
-
+    protected Vector3[] startDirectionSucc;
+    protected Quaternion[] startRotationBone;
+    protected Quaternion startRotationTarget;
+    protected Quaternion startRotationRoot;
 
     // Start is called before the first frame update
     void Awake()
@@ -32,21 +35,26 @@ public class FastIKFabric : MonoBehaviour
         bones = new Transform[chainLength + 1];
         positions = new Vector3[chainLength + 1];
         bonesLength = new float[chainLength];
+        startDirectionSucc = new Vector3[chainLength + 1];
+        startRotationBone = new Quaternion[chainLength + 1];
 
+        startRotationTarget = target.transform.rotation;
         completeLength = 0;
 
         Transform current = transform;
-        for (int i = bones.Length - 1; i >= 0 - 1; i--)
+        for (int i = bones.Length - 1; i >= 0; i--)
         {
             bones[i] = current;
+            startRotationBone[i] = current.rotation;
 
             if (i == bones.Length - 1)
             {
-
+                startDirectionSucc[i] = target.transform.position - current.position;
             }
             else 
             {
-                bonesLength[i] = (bones[i + 1].position - current.position).magnitude;
+                startDirectionSucc[i] = bones[i + 1].position - current.position;
+                bonesLength[i] = startDirectionSucc[i].magnitude;
                 completeLength += bonesLength[i];
             }
 
@@ -67,9 +75,91 @@ public class FastIKFabric : MonoBehaviour
         if (bonesLength.Length != chainLength)
             Init();
 
+        // Get position
         for (int i = 0; i < bones.Length; i++)
         {
             positions[i] = bones[i].position;
+        }
+
+        Quaternion rootRot = (bones[0].parent != null) ? bones[0].parent.rotation : Quaternion.identity;
+        Quaternion rootRotDiff = rootRot * Quaternion.Inverse(startRotationRoot);
+
+        // Calculations
+        // Further away than max length
+        if ((target.transform.position - bones[0].position).sqrMagnitude >= completeLength * completeLength)
+        {
+            // Stretch it
+            Vector3 direction = (target.transform.position - positions[0]).normalized;
+
+            // Set everything after root
+            for (int i = 1; i < positions.Length; i++)
+            {
+                positions[i] = positions[i - 1] + direction * bonesLength[i - 1];
+            }
+        }
+        // Closer than max length
+        else 
+        {
+            for (int i = 0; i < positions.Length - 1; i++)
+            {
+                positions[i + 1] = Vector3.Lerp(positions[i + 1], positions[i] + rootRotDiff * startDirectionSucc[i], snapBackStrength);
+            }
+
+            for (int iteration = 0; iteration < iterations; iteration++)
+            {
+                // Backward algorithm
+                for (int i = positions.Length - 1; i > 0; i--)
+                {
+                    if (i == positions.Length - 1)
+                    {
+                        // Set leaf bone to target position
+                        positions[i] = target.transform.position;
+                    }
+                    else 
+                    {
+                        positions[i] = positions[i + 1] + (positions[i] - positions[i + 1]).normalized * bonesLength[i];
+                    }
+                }
+
+                // Forward algorithm
+                for (int i = 1; i < positions.Length; i++)
+                {
+                    positions[i] = positions[i - 1] + (positions[i] - positions[i - 1]).normalized * bonesLength[i - 1];
+                }
+
+                // Stop calculations if leaf bone is close enough to target
+                if ((positions[positions.Length - 1] - target.transform.position).sqrMagnitude < delta * delta)
+                {
+                    break;
+                }
+            }
+
+            // Pole calculations
+            if (pole != null)
+            {
+                for (int i = 1; i < positions.Length - 1; i++)
+                {
+                    Plane plane = new Plane(positions[i + 1] - positions[i - 1], positions[i - 1]);
+                    Vector3 projectedPole = plane.ClosestPointOnPlane(pole.transform.position);
+                    Vector3 projectedBone = plane.ClosestPointOnPlane(positions[i]);
+                    float angle = Vector3.SignedAngle(projectedBone - positions[i - 1], projectedPole - positions[i - 1], plane.normal);
+                    positions[i] = Quaternion.AngleAxis(angle, plane.normal) * (positions[i] - positions[i - 1]) + positions[i - 1];
+                }
+            }
+        }
+
+        // Set position & rotation
+        for (int i = 0; i < bones.Length; i++)
+        {
+            if (i == positions.Length - 1)
+            {
+                bones[i].rotation = target.transform.rotation * Quaternion.Inverse(startRotationTarget) * startRotationBone[i];
+            }
+            else 
+            {
+                bones[i].rotation = Quaternion.FromToRotation(startDirectionSucc[i], positions[i + 1] - positions[i]) * startRotationBone[i];
+            }
+            bones[i].position = positions[i];
         }
     }
 
